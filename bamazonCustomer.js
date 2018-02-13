dcfc428bdf0fffbc11007286cb3c22ac7cd90b21
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-13 01:06:49 -0600

    adding table need to figure out how to parse SQL data so no more RowDataPacket

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,25 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var products_table = new Table({
+  head: ["Item Id", "Product Name", "Department Name", "Price", "# In Stock"]
+});
+
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bamazon_db'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bamazon_db"
 });
 
 connection.connect();
+
+connection.query("SELECT * FROM products as solution", function( error,results) {
+  if (error) throw error;
+
+  console.log(products_table.toString());
+  console.log(results[0]);
+});
+
+connection.end();
