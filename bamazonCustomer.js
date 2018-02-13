f4afda7ac2cf9bbc23e2a94f52ac174c404bfed7
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-13 11:43:06 -0600

    figuring some inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
+var colors= require('colors');
 
 var products_table = new Table({
-  head: ["Item Id", "Product Name", "Department Name", "Price", "# In Stock"]
+  head: ["Item Id".cyan, "Product Name".cyan, "Department Name".cyan, "Price".cyan, "# In Stock".cyan]
 });
 
 var connection = mysql.createConnection({
@@ -18,12 +19,26 @@ connection.connect();
 connection.query("SELECT * FROM products as solution", function(error,results) {
   if (error) throw error;
   results.forEach(product => pushTotable(product));
-
   console.log(products_table.toString());
+  inquirer.prompt([{
+      type: "list", 
+      message: "What is the ID of the product you want to buy?",
+      name:"productChoice", 
+      choices: ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10"]
+    }, {
+      type: "input", 
+      message: "How many units would you like to buy?",
+      name: "unitsBought"
+    }
+    ]).then(function(inquirerResponse){
+      //note to self returns a string need parseInt to turn back to numbah
+      console.log(+inquirerResponse.productChoice + +inquirerResponse.unitsBought);
+    } 
+    )
 });
 
 connection.end();
 
-function pushTotable (element) {
-  products_table.push([element.id, element.product_name, element.department_name, element.price, element.stock_quantity]);
+function pushTotable(element) {
+  products_table.push([element.id, element.product_name, element.department_name,` $ ${element.price}`, element.stock_quantity]);
 }
