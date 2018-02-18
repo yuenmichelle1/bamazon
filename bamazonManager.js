af7ef8058e888472fe1f265d93f6ef3d0c5e6dbb
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-17 21:41:49 -0600

    fixed third function next up is adding new product

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,12 +2,23 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
 var colors = require("colors");
-var commandsArr = ["View Products for Sale","View Low Inventory","Add To Inventory","Add New Product"];
+var commandsArr = [
+  "View Products for Sale",
+  "View Low Inventory",
+  "Add To Inventory",
+  "Add New Product"
+];
 var productsArr;
 
 //Create a table with headings: item id, product name, department name, price, quantity in a pretty blue color
 var products_table = new Table({
-  head: ["Item Id".cyan, "Product Name".cyan, "Department Name".cyan, "Price".cyan,"# In Stock".cyan]
+  head: [
+    "Item Id".cyan,
+    "Product Name".cyan,
+    "Department Name".cyan,
+    "Price".cyan,
+    "# In Stock".cyan
+  ]
 });
 
 var connection = mysql.createConnection({
@@ -38,7 +49,6 @@ function inquireManager() {
     .then(function(inquirerResponse) {
       var managerCommand = inquirerResponse.managerJob;
       doCommand(managerCommand);
-      // console.log(managerCommand);
     });
 }
 
@@ -50,14 +60,14 @@ function doCommand(command) {
       break;
     //CASE: VIEW LOW INVENTORY
     case commandsArr[1]:
-        showLowInventory();
-        break;
-    //CASE: ADD TO INVENTORY    
+      showLowInventory();
+      break;
+    //CASE: ADD TO INVENTORY
     case commandsArr[2]:
       inquireItemtoRestock();
       break;
     default:
-      break;    
+      break;
   }
 }
 
@@ -93,34 +103,93 @@ function showProductstable() {
 }
 
 function pushTotable(table, element) {
-  table.push([element.id, element.product_name, element.department_name, ` $ ${element.price}`, element.stock_quantity]);
+  table.push([
+    element.id,
+    element.product_name,
+    element.department_name,
+    ` $ ${element.price}`,
+    element.stock_quantity
+  ]);
 }
 
-function showLowInventory(){
-    var lowStock_table = new Table({
-        head: ["Item Id".red, "Product Name".red, "Department Name".red, "Price".red,"# In Stock".red]
-    });
-    connection.query("SELECT * FROM products as solution WHERE stock_quantity < 5", function(error, results) {
-        if (error) throw error;
-        var productsArray = results;
-        productsArray.forEach(product => pushTotable(lowStock_table, product));
-        console.log(lowStock_table.toString());
-        askToDoAgain();
+function showLowInventory() {
+  var lowStock_table = new Table({
+    head: [
+      "Item Id".red,
+      "Product Name".red,
+      "Department Name".red,
+      "Price".red,
+      "# In Stock".red
+    ]
+  });
+  connection.query(
+    "SELECT * FROM products as solution WHERE stock_quantity < 5",
+    function(error, results) {
+      if (error) throw error;
+      var productsArray = results;
+      productsArray.forEach(product => pushTotable(lowStock_table, product));
+      console.log(lowStock_table.toString());
+      askToDoAgain();
+    }
+  );
+}
+
+function inquireItemtoRestock(arr) {
+  connection.query("SELECT * FROM products as solution", function(
+    error,
+    results
+  ) {
+    if (error) throw error;
+    productsArr = results;
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What is the Product Id you want to restock?",
+          name: "itemRestockid",
+          choices: productsArr.map(product => `${product.id}`)
+        },
+        {
+          type: "input",
+          message: "How many Units do you want to add?",
+          name: "units",
+          validate: validateNumber
+        }
+      ])
+      .then(function(inquirerResponse) {
+        var unitsToAdd = +inquirerResponse.units;
+        var productId = +inquirerResponse.itemRestockid;
+        connection.query(`SELECT stock_quantity FROM products WHERE id = ${productId}`, function (err, results){
+          if (err) throw err;
+          var currentStock = +results[0].stock_quantity;
+          addProduct(unitsToAdd + currentStock, productId);
+        })
       });
+  });
 }
 
-function inquireItemtoRestock() {
-  inquirer.prompt([{
-    type: "list",
-    message: "What is the Product Id you want to restock?",
-    name: "itemRestockid", 
-    choices: productsArr.map(product => `${product.id}`)
-  }]).then(function(inquirerResponse){
-    console.log(inquirerResponse.itemRestockid);
-    askToDoAgain();
-  })
+function addProduct(units, id) {
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: units
+      },
+      {
+        id: id
+      }
+    ],
+    function(error, results, fields) {
+      if (error) throw error;
+      console.log(`Your item has been restocked new stock quantity for Product Id ${id} is ${units}`);
+      askToDoAgain();
+    }
+  );
 }
 
-
+function validateNumber(userInput) {
+  var reg = /^\d+$/;
+  return reg.test(userInput) || "Should be a number!";
+}
 
 inquireManager();
